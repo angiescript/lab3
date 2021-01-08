ff409c86d07e5a43f46b5d26007c24e649e159fb
Anna Blankell <anna.blankell@outlook.com>
2021-01-08 12:00:03 +0100

    add style and rename

--- script.js
+++ script.js
@@ -1,34 +1,29 @@
-/* TODO:
-1. Catch?
-4. Styla allt. */
-
-const apikey = "4831f8f1";
+const apiKey = "4831f8f1";
 
 document.addEventListener("DOMContentLoaded", (e) => {
-  let inputfield = document.getElementById("movie");
-  let datadiv = document.getElementById("movies");
+  let inputfield = document.getElementById("inputSearch");
+  let searchResults = document.getElementById("searchResults");
   let noResult = document.getElementById("error-txt");
-  let infobox = document.getElementById("info");
+  let infobox = document.getElementById("infoBox");
 
   inputfield.addEventListener("input", (e) => {
     removeInfo();
     noResult.classList.add("hidden");
 
-    while (datadiv.firstChild) {
-      datadiv.removeChild(datadiv.lastChild);
+    while (searchResults.firstChild) {
+      searchResults.removeChild(searchResults.lastChild);
     }
 
     if (inputfield.value.length >= 3) {
-      //let space = inputfield.value.replace(/ /g, "*");
-      let space = inputfield.value.trim();
+      let trimmedValue = inputfield.value.trim();
 
-      fetch(`http://www.omdbapi.com/?s=${space}*&apikey=${apikey}&`)
+      fetch(`http://www.omdbapi.com/?s=${trimmedValue}*&apikey=${apiKey}&`)
         .then((resp) => {
           return resp.json();
         })
         .then((data) => {
-          while (datadiv.firstChild) {
-            datadiv.removeChild(datadiv.lastChild);
+          while (searchResults.firstChild) {
+            searchResults.removeChild(searchResults.lastChild);
           }
 
           if (!data.Search) {
@@ -41,22 +36,20 @@ document.addEventListener("DOMContentLoaded", (e) => {
 
           if (data.Search) {
             for (let i = 0; i < data.Search.length; i++) {
-              let moviep = document.createElement("p");
-              moviep.setAttribute(`id`, `movie${i + 1}`);
-              moviep.innerHTML = data.Search[i].Title;
-              datadiv.appendChild(moviep);
+              let movieP = document.createElement("p");
+              movieP.setAttribute(`id`, `movie${i + 1}`);
+              movieP.innerHTML = data.Search[i].Title;
+              searchResults.appendChild(movieP);
             }
           }
 
-          let moviearray = Array.from(datadiv.childNodes);
-          console.log(moviearray);
-          console.log(typeof moviearray);
+          let movieArray = Array.from(searchResults.childNodes);
 
-          for (let i = 0; i < moviearray.length; i++) {
-            moviearray[i].addEventListener("click", (e) => {
+          for (let i = 0; i < movieArray.length; i++) {
+            movieArray[i].addEventListener("click", (e) => {
               removeInfo();
               let imdb_id = data.Search[i].imdbID;
-              fetch(`http://www.omdbapi.com/?i=${imdb_id}&apikey=${apikey}&`)
+              fetch(`http://www.omdbapi.com/?i=${imdb_id}&apikey=${apiKey}&`)
                 .then((resp) => {
                   return resp.json();
                 })
@@ -67,32 +60,40 @@ document.addEventListener("DOMContentLoaded", (e) => {
                   poster.src = `${data.Poster}`;
                   infobox.appendChild(poster);
 
-                  let titlep = document.createElement("p");
-                  titlep.innerHTML = `<span>${data.Title}</span>`;
-                  infobox.appendChild(titlep);
+                  let titleP = document.createElement("p");
+                  titleP.innerHTML = `<span>${data.Title}</span>`;
+                  infobox.appendChild(titleP);
 
-                  let yearp = document.createElement("p");
-                  yearp.innerHTML = `<span>År:</span> ${data.Year}`;
-                  infobox.appendChild(yearp);
+                  let yearP = document.createElement("p");
+                  yearP.innerHTML = `<span>År:</span> ${data.Year}`;
+                  infobox.appendChild(yearP);
 
-                  let genrep = document.createElement("p");
-                  genrep.innerHTML = `<span>Genre:</span> ${data.Genre}`;
-                  infobox.appendChild(genrep);
+                  let genreP = document.createElement("p");
+                  genreP.innerHTML = `<span>Genre:</span> ${data.Genre}`;
+                  infobox.appendChild(genreP);
 
-                  let plotp = document.createElement("p");
-                  plotp.innerHTML = `<span>Handling: </span>${data.Plot}`;
-                  infobox.appendChild(plotp);
+                  let plotP = document.createElement("p");
+                  plotP.innerHTML = `<span>Handling: </span>${data.Plot}`;
+                  infobox.appendChild(plotP);
 
-                  let imdblinkp = document.createElement("p");
-                  imdblinkp.innerHTML = `<a href="https://www.imdb.com/title/${data.imdbID}/" target="blank">IMDB-länk</a>`;
-                  infobox.appendChild(imdblinkp);
+                  let imdbP = document.createElement("p");
+                  imdbP.innerHTML = `<a href="https://www.imdb.com/title/${data.imdbID}/" target="blank"><img src ="imdb.png" width="150px"></a>`;
+                  infobox.appendChild(imdbP);
+                })
+
+                .catch((error) => {
+                  console.log(`Something went wrong: ${error}`);
+                  infobox.classList.remove("hidden");
+                  infobox.innerHTML = `<span class="errorInfo">Något gick fel: ${error}</span>`;
                 });
             });
           }
         })
 
         .catch((error) => {
-          console.log(`${error}`);
+          console.log(`Something went wrong: ${error}`);
+          noResult.innerHTML = `Något gick fel: ${error}`;
+          noResult.classList.remove("hidden");
         });
     }
   });
