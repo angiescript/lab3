2df6219f01e06e703aecf4667d21edacfed11ad6
Anna Blankell <anna.blankell@outlook.com>
2021-01-08 11:03:17 +0100

    change style.

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 /* TODO:
 1. Catch?
-2. Ta bort filmer i diven som redan ligger i infobox när man gör en ny sökning ELLER klickar på en ny film.
-3. Classer?
 4. Styla allt. */
 
 const apikey = "4831f8f1";
@@ -11,10 +9,9 @@ document.addEventListener("DOMContentLoaded", (e) => {
   let datadiv = document.getElementById("movies");
   let noResult = document.getElementById("error-txt");
   let infobox = document.getElementById("info");
- 
 
   inputfield.addEventListener("input", (e) => {
-    
+    removeInfo();
     noResult.classList.add("hidden");
 
     while (datadiv.firstChild) {
@@ -45,66 +42,62 @@ document.addEventListener("DOMContentLoaded", (e) => {
           if (data.Search) {
             for (let i = 0; i < data.Search.length; i++) {
               let moviep = document.createElement("p");
-              moviep.setAttribute(`id`, `movie${i+1}`)
+              moviep.setAttribute(`id`, `movie${i + 1}`);
               moviep.innerHTML = data.Search[i].Title;
               datadiv.appendChild(moviep);
-
             }
           }
 
           let moviearray = Array.from(datadiv.childNodes);
-          console.log(moviearray)
-          console.log(typeof moviearray)
-          
-          for (let i=0; i < moviearray.length; i++) {
+          console.log(moviearray);
+          console.log(typeof moviearray);
+
+          for (let i = 0; i < moviearray.length; i++) {
             moviearray[i].addEventListener("click", (e) => {
-
-              let imdb_id = data.Search[i].imdbID
+              removeInfo();
+              let imdb_id = data.Search[i].imdbID;
               fetch(`http://www.omdbapi.com/?i=${imdb_id}&apikey=${apikey}&`)
-              .then((resp) => {
-                return resp.json();
-              })
-              .then(data => {
-                infobox.classList.remove("hidden");
+                .then((resp) => {
+                  return resp.json();
+                })
+                .then((data) => {
+                  infobox.classList.remove("hidden");
 
-                let poster = document.createElement("img");
-                poster.src = `${data.Poster}`
-                infobox.appendChild(poster);
+                  let poster = document.createElement("img");
+                  poster.src = `${data.Poster}`;
+                  infobox.appendChild(poster);
 
-                let titlep = document.createElement("p");
-                titlep.innerHTML = data.Title;
-                infobox.appendChild(titlep);
+                  let titlep = document.createElement("p");
+                  titlep.innerHTML = `<span>${data.Title}</span>`;
+                  infobox.appendChild(titlep);
 
-                let yearp = document.createElement("p");
-                yearp.innerHTML = (`År: ${data.Year}`)
-                infobox.appendChild(yearp);
+                  let yearp = document.createElement("p");
+                  yearp.innerHTML = `<span>År:</span> ${data.Year}`;
+                  infobox.appendChild(yearp);
 
-                let genrep = document.createElement("p");
-                genrep.innerHTML = (`Genre: ${data.Genre}`);
-                infobox.appendChild(genrep);
-                
-                let plotp = document.createElement("p");
-                plotp.innerHTML = (`Handling: ${data.Plot}`);
-                infobox.appendChild(plotp);
+                  let genrep = document.createElement("p");
+                  genrep.innerHTML = `<span>Genre:</span> ${data.Genre}`;
+                  infobox.appendChild(genrep);
 
-                let imdblinkp = document.createElement("p");
-                imdblinkp.innerHTML = (`<a href="https://www.imdb.com/title/${data.imdbID}/" target="blank">IMDB-länk</a>`);
-                infobox.appendChild(imdblinkp);
+                  let plotp = document.createElement("p");
+                  plotp.innerHTML = `<span>Handling: </span>${data.Plot}`;
+                  infobox.appendChild(plotp);
 
-              })
-            })
+                  let imdblinkp = document.createElement("p");
+                  imdblinkp.innerHTML = `<a href="https://www.imdb.com/title/${data.imdbID}/" target="blank">IMDB-länk</a>`;
+                  infobox.appendChild(imdblinkp);
+                });
+            });
           }
-
         })
 
         .catch((error) => {
           console.log(`${error}`);
         });
-   
     }
   });
-
-  
-  
-
+  let removeInfo = () => {
+    infobox.innerHTML = "";
+    infobox.classList.add("hidden");
+  };
 });
